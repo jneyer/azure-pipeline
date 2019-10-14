5401b757f8468d8318a3f40d3888c7eebad1fd00
John Neyer <jneyer@captechconsulting.com>
2019-10-14 15:08:48 -0400

    Adding cli tool

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,8 @@ echo "AGENT_BUILDDIRECTORY is $AGENT_BUILDDIRECTORY"
 echo "AGENT_BUILDDIRECTORY contents:"
 ls -1 $AGENT_BUILDDIRECTORY
 echo "SYSTEM_HOSTTYPE is $SYSTEM_HOSTTYPE"
+
+java -jar ./virtserver-cli.jar --help
+
+
 echo "Over and out."
